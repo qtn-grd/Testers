b3e5a9f7e5b9473f410f95f22b6d6f5c758aee7c
Quentin <quentin.gairaud@gmail.com>
2026-01-06 21:53:22 +0100

    maj gnl tester

--- Tests_GNL/Test_GNL.c
+++ Tests_GNL/Test_GNL.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                    :+:      :+:    :+:   */
+/*   Test_GNL.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qgairaud <qgairaud@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 21:08:53 by qgairaud          #+#    #+#             */
-/*   Updated: 2025/12/05 21:09:13 by qgairaud         ###   ########lyon.fr   */
+/*   Updated: 2026/01/06 21:44:09 by quentin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,35 +58,61 @@
 // 	return (0);
 // }
 
-/// POUR VERIFIER L'ABSENCE DE FUITES DE MEMOIRE ///
-
-// int	main(void)
-// {
-// 	char	*line;
-// 	int		fd;
-
-// 	fd = open("test.txt", O_RDONLY);
-// 	if (fd < 0)
-// 		return (1);
-// 	while ((line = get_next_line(fd)) != NULL)
-// 	{
-// 		free(line);
-// 	}
-// 	close(fd);
-// 	return (0);
-// }
 
 /// POUR TESTER STDIN ///
 /// penser à rediriger aussi un fichier < ///
 
-int	main(void)
-{
-	char	*line;
+// int	main(void)
+// {
+// 	char	*line;
 
-	while ((line = get_next_line(0)))
-	{
-		printf("LINE = %s", line);
-		free(line);
-	}
-	return (0);
-}
+// 	while ((line = get_next_line(0)))
+// 	{
+// 		printf("LINE = %s", line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
+
+/// POUR TESTER des bonus ///
+/// et lire deux fichiers en entier en alternant leurs lignes///
+
+// #include "get_next_line_bonus.h"
+
+// int     main(void)
+// {
+//     int     fd1;
+//     int     fd2;
+//     int     x;
+//     char    *line1;
+//     char    *line2;
+
+//     x = 0;
+//     fd1 = open("my_file1.txt", O_RDONLY);
+//     fd2 = open("my_file2.txt", O_RDONLY);
+//     if (fd1 < 0 || fd2 < 0)
+//         return (1);
+//     while (x < 5)
+//     {
+//         line1 = get_next_line(fd1);
+//         line2 = get_next_line(fd2);
+
+//         if (!line1 && !line2)
+//             break;
+
+//         if (line1)
+//         {
+//             printf("%s", line1);
+//             free(line1);
+//         }
+//         if (line2)
+//         {
+//             printf("%s", line2);
+//             free(line2);
+//         }
+//         x++;
+//     }
+//     close(fd1);
+//     close(fd2);
+//     return (0);
+// }
